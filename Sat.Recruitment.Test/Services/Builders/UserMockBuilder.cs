beeda112e20ba2ca1d2b51b4f9e70ea50b6fc60f
lunajfranco <lunajfranco@gmail.com>
2023-08-24 18:02:51 -0300

    improve unit test

---
+++ Sat.Recruitment.Test/Services/Builders/UserMockBuilder.cs
@@ -0,0 +1,29 @@
+﻿using Sat.Recruitment.Api.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sat.Recruitment.Test.Services.Builders
+{
+    public class UserMockBuilder
+    {
+        public List<User> BuildUserMock()
+        {
+            return new List<User>() { new User() { 
+                Name = "Mike", 
+                Email = "mike@gmail.com", 
+                Address = "Av. Juan G", 
+                Phone = "+349 1122354215", 
+                Money = 124, 
+                UserType = "Normal" }, 
+                new User() { 
+                Name = "Agustina",
+                Email = "Agustina@gmail.com",
+                Address = "Av. Juan G",
+                Phone = "+349 1122354215",
+                Money = 124,
+                UserType = "Normal"
+                }};
+        }
+    }
+}
--- Sat.Recruitment.Test/UnitTest1.cs
+++ Sat.Recruitment.Test/UnitTest1.cs
@@ -2,7 +2,7 @@ using Sat.Recruitment.Api.Controllers;
 using Sat.Recruitment.Api.Models.Entities;
 using Sat.Recruitment.Api.Services.Interfaces;
 using Sat.Recruitment.Api.Services.Services;
-using Sat.Recruitment.Test.Services.Mock;
+using Sat.Recruitment.Test.Services.Builders;
 using Xunit;
 
 namespace Sat.Recruitment.Test
@@ -10,18 +10,25 @@ namespace Sat.Recruitment.Test
     [CollectionDefinition("Tests", DisableParallelization = true)]
     public class UnitTest1
     {
+        private readonly UsersController _userController;
+        private readonly IUserService _userService;
+        private readonly IValidateUserErrors _validateUserErrors;
+        private readonly IStreamReaderService _streamReaderService;
+        private readonly UserMockBuilder _userMockBuilder;
+        public UnitTest1()
+        {
+            _userMockBuilder = new UserMockBuilder();
+            _streamReaderService = new StreamReaderService();
+            _userService = new UserService(_streamReaderService);
+            _validateUserErrors = new ValidateUserErrors();
+            _userController = new UsersController(_userService, _validateUserErrors);
+        }
         [Fact]
         public void Test1()
         {
-            IStreamReaderService streamReaderService = new StreamReaderService();
-            IUserService userService = new UserServiceMock(streamReaderService);
-            IValidateUserErrors validateUserService = new ValidateUserServiceMock();
+            var user = _userMockBuilder.BuildUserMock()[0];
 
-            var userController = new UsersController(userService, validateUserService);
-
-            var user = new User() { Name = "Mike", Email = "mike@gmail.com", Address = "Av. Juan G", Phone = "+349 1122354215", Money = 124, UserType = "Normal" };
-            var result = userController.CreateUser(user).Result;
-
+            var result = _userController.CreateUser(user).Result;
 
             Xunit.Assert.Equal(true, result.IsSuccess);
             Xunit.Assert.Equal("User Created", result.Errors);
@@ -30,21 +37,9 @@ namespace Sat.Recruitment.Test
         [Fact]
         public void Test2()
         {
-            IStreamReaderService streamReaderService = new StreamReaderService();
-            IUserService userService = new UserServiceMock(streamReaderService);
-            IValidateUserErrors validateUserService = new ValidateUserServiceMock();
-            var userController = new UsersController(userService, validateUserService);
-            var user = new User()
-            {
-                Name = "Agustina",
-                Email = "Agustina@gmail.com",
-                Address = "Av. Juan G",
-                Phone = "+349 1122354215",
-                Money = 124,
-                UserType = "Normal"
-            };
-            var result = userController.CreateUser(user).Result;
+            var user = _userMockBuilder.BuildUserMock()[1];
 
+            var result = _userController.CreateUser(user).Result;
 
             Xunit.Assert.Equal(false, result.IsSuccess);
             Xunit.Assert.Equal("The user is duplicated", result.Errors);
